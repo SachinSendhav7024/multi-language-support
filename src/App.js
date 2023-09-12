9ec9bba8bdffe48df00f42e98d6ac699994562d0
SachinSendhav7024 <sachinsendhav7024@gmail.com>
2023-09-13 01:25:53 +0530

    fix responsive issue

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import logo from "./assets/languageicon.png";
 const App = () => {
   return (
     <>
-      <div className="container-fluid main_page">
+      <div className="container-fluid main_page pb-4">
         <div className="navbar">
           <img src={logo} alt="languagelogo" className="logo mx-5 p-4" />
           <Navbar />
